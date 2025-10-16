68e317ac3a75951e9c8d0f25c549c9bd68e65a0b
Dana Halabi <d3hhalabi@gmail.com>
2025-10-16 20:18:12 +0300

    3-08-fetching-products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import { ProductCard } from "@/components/ProductCard";
-import { mockProducts } from "@/lib/mocks";
+import { prisma } from "@/lib/prisma";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const products = await prisma.product.findMany();
   return (
     <main className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Home</h1>
-      <p>Showing {mockProducts.length} products</p>
+      <p>Showing {products.length} products</p>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {mockProducts.map((product) => (
+        {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,17 +1,20 @@
-import { formatPrice, Product } from "@/lib/mocks";
+import { formatPrice } from "@/lib/mocks";
 import Image from "next/image";
+import { Product } from "@/generated/prisma";
 
 export function ProductCard({ product }: { product: Product }) {
   return (
     <div className="border border-gray-200 rounded-lg p-4">
       <div className="relative aspect-video">
-        <Image
-          src={product.image}
-          alt={product.name}
-          className="object-cover"
-          fill
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        />
+        {product.image && (
+          <Image
+            src={product.image}
+            alt={product.name}
+            className="object-cover"
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        )}
       </div>
       <h2 className="text-lg font-semibold">{product.name}</h2>
       <p className="text-gray-600">{formatPrice(product.price)}</p>
