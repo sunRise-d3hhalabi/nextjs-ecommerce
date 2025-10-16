ce8b37a60e8ebc5a02be2c41c002056b35fbd721
Dana Halabi <d3hhalabi@gmail.com>
2025-10-16 19:56:11 +0300

    3-05-using-prisma

--- src/lib/seed.ts
+++ src/lib/seed.ts
@@ -1,1 +1,41 @@
 console.log("Seeding database...");
+import { PrismaClient } from "@/generated/prisma";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+
+  const electronics = await prisma.category.create({
+    data: {
+      name: "Electronics",
+      slug: "electronics",
+    },
+  });
+
+  const clothing = await prisma.category.create({
+    data: {
+      name: "Clothing",
+      slug: "clothing",
+    },
+  });
+
+  const home = await prisma.category.create({
+    data: {
+      name: "Home",
+      slug: "home",
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    console.log("Seeding complete!");
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
